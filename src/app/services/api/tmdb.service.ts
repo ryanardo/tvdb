8398c6aec93328734d60ae77bf8db36565712ecf
Ryan G Ferris <ryanglenferris@gmail.com>
2018-03-05 15:42:29 -0800

    TMDB API Query
    Update the TMDB API Query parameters.

--- src/app/services/api/tmdb.service.ts
+++ src/app/services/api/tmdb.service.ts
@@ -4,12 +4,16 @@ import { API_KEY } from './../../api-keys';
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
-export class TMDB {
+export class TMDB_API {
 
 	constructor(private http: Http) { }
 
-	getMultiSearch(query: string, page_number: number, include_adult: boolean) {
-		return this.http.get(`https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&language=en-US&query=${query}&page=${page_number}&include_adult=${include_adult}`);
+	// getMultiSearch(query: string, page_number: number, include_adult: string) {
+	// 	return this.http.get(`https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&language=en-US&query=${query}&page=${page_number}&include_adult=${include_adult}`);
+	// }
+
+	getMultiSearch(query: string, include_adult: string) {
+		return this.http.get(`https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=${include_adult}`);
 	}
 
 } //End of the 'TMDB' Constructor.
