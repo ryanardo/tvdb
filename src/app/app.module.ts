99e7660b4e1bcced069029ab3a1740dcf0e15421
Ryan G Ferris <ryanglenferris@gmail.com>
2018-03-05 08:58:45 -0800

    App Module
    Update the App Module file after installing the Angular Components using
    Angular CLI commands.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { ShowDetailComponent } from './components/show-detail/show-detail.component';
+import { SearchComponent } from './components/search/search.component';
+import { UserFavoritesComponent } from './components/user-favorites/user-favorites.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    ShowDetailComponent,
+    SearchComponent,
+    UserFavoritesComponent
   ],
   imports: [
     BrowserModule,
