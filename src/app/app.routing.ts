0c868b70690a46497a643c40e0280ca570321d21
Ryan G Ferris <ryanglenferris@gmail.com>
2018-03-06 15:23:16 -0800

    App Routing
    Add the TV and Movie Components Routes to the app.routing.ts file.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,14 +1,23 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 
+import { MovieDetailComponent } from './components/movie-detail-page/movie-detail-page.component';
 import { SearchFormComponent } from './components/search-form/search-form.component';
+import { TvDetailPageComponent } from './components/tv-detail-page/tv-detail-page.component';
 
 const appRoutes: Routes = [
 	{
 		path: '',
 		component: SearchFormComponent
+	},
+	{
+		path: 'movie/:id',
+		component: MovieDetailComponent
+	},
+	{
+		path: 'tv/:id',
+		component: TvDetailPageComponent
 	}
 ];
 
