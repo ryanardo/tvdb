141ac814af7be9328a142590dfa22c5d4ab7174c
danyel murray <danyel@danyels-MacBook-Pro.local>
2018-03-06 13:06:09 -0800

    add images and video to dashboard

--- src/app/layout/dashboard/dashboard.component.ts
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -14,22 +14,47 @@ export class DashboardComponent implements OnInit {
     constructor() {
         this.sliders.push(
             {
-                imagePath: 'assets/images/slider1.jpg',
-                label: 'First slide label',
+                imagePath: 'assets/images/alien.jpg',
+                label: 'Watch your favorite movies',
                 text:
-                    'Nulla vitae elit libero, a pharetra augue mollis interdum.'
+                    ''
             },
             {
-                imagePath: 'assets/images/slider2.jpg',
-                label: 'Second slide label',
-                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+                imagePath: 'assets/images/300.jpg',
+                label: '',
+                text: ''
             },
             {
-                imagePath: 'assets/images/slider3.jpg',
-                label: 'Third slide label',
+                imagePath: 'assets/images/startrek.jpg',
+                label: '',
                 text:
-                    'Praesent commodo cursus magna, vel scelerisque nisl consectetur.'
-            }
+                    ''
+            },
+            {
+                imagePath: 'assets/images/rushmore.jpg',
+                label: '',
+                text: ''
+            },
+            {
+                imagePath: 'assets/images/kong.jpg',
+                label: '',
+                text: ''
+            },
+            {
+                imagePath: 'assets/images/leo.jpg',
+                label: '',
+                text: ''
+            },
+            {
+                imagePath: 'assets/images/guardians.png',
+                label: '',
+                text: ''
+            },
+            {
+                imagePath: 'assets/images/fal.jpg',
+                label: '',
+                text: ''
+            },
         );
 
         this.alerts.push(
