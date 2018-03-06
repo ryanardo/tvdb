23594ac4fb09ae2a3964768b0283be4be2b3eb8c
danyel murray <danyel@danyels-MacBook-Pro.local>
2018-03-06 14:58:10 -0800

    add app routes.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,12 +4,34 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 import { SearchFormComponent } from './components/search-form/search-form.component';
+import { LoginComponent } from './components/login/login.component';
+import { ShowDetailComponent } from './components/show-detail/show-detail.component';
+import { UserFavoritesComponent } from './components/user-favorites/user-favorites.component';
+import { HomeComponent } from './components/home/home.component';
+
 
 const appRoutes: Routes = [
 	{
 		path: '',
+		component: HomeComponent
+	},
+	{
+		path: 'search',
 		component: SearchFormComponent
+	},
+	{
+		path: 'login',
+		component: LoginComponent
+	},
+	{
+		path: 'favorites',
+		component: UserFavoritesComponent
+	},
+	{
+		path: 'browse',
+		component: ShowDetailComponent
 	}
+
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
