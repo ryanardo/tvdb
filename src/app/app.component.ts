f1c7a0236f0c47ffe839f0e6ca4651fc09840563
danyel murray <danyel@danyels-MacBook-Pro.local>
2018-03-07 15:09:30 -0800

    style login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,8 @@ import { routing } from './app.routing';
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css']
+	styleUrls: ['./app.component.css'] 
+
 })
 
 export class AppComponent {
