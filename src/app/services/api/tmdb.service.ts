e0d6dd68be1196a13f76d3654e4f607202107cca
Guest User <Guest@Epicodus-4B.local>
2018-03-07 11:48:57 -0800

    add tv detail page functionality

--- src/app/services/api/tmdb.service.ts
+++ src/app/services/api/tmdb.service.ts
@@ -12,10 +12,10 @@ export class TMDB_API {
 		return this.http.get(`https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=${include_adult}`);
 	}
 	getMovieById(id: string) {
-		return this.http.get(`https://api.themoviedb.org/3/movie/{movie_id}?api_key=${API_KEY}&language=en-US`);
+		return this.http.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`);
 	}
 	getTVById(id: string) {
-		return this.http.get(`https://api.themoviedb.org/3/tv/{movie_id}?api_key=${API_KEY}&language=en-US`);
+		return this.http.get(`https://api.themoviedb.org/3/tv/${id}?api_key=${API_KEY}&language=en-US`);
 	}
 
 	getMovieNowPlaying(id: number) {
