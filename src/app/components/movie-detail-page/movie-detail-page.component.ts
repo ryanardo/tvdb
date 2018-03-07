f16bc367d33146e9e9b90578e9890287a0d016c7
Guest User <Guest@Epicodus-4B.local>
2018-03-07 11:57:58 -0800

    add movie detail functionality

--- src/app/components/movie-detail-page/movie-detail-page.component.ts
+++ src/app/components/movie-detail-page/movie-detail-page.component.ts
@@ -14,19 +14,18 @@ import { TMDB_API } from './../../services/api/tmdb.service';
 export class MovieDetailComponent implements OnInit {
 
 	public href: string = "";
+	public id: string="";
+	public example;
 
 	constructor(private router: Router, private tmdb_api: TMDB_API) { }
 
 	ngOnInit() {
 		this.href = this.router.url;
-		var id = this.href.substr(7, 12);
+		//this.id = this.href.substr(4, 12);
+		 this.tmdb_api.getMovieById(this.href.substr(7, 12)).subscribe(response => {
+			this.example = response.json();
+			console.log(this.example)
+			});
 	}
 
-	getDetails(id: string) {
-		this.tmdb_api.getMovieById(id).subscribe(response => {
-			var details = response.json();
-			console.log(details);
-		});
-
-	}
 }
