81f202ede90d08da19ee9a1d647d6b0ddac6fcda
Rodnier Borrego <rodnierbc89@gmail.com>
2018-03-06 16:13:25 -0800

    Login Component
    Update the Login Component to Authenticate Users in Firebase.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -43,7 +43,7 @@ export class LoginComponent implements OnInit {
     signInWithGithub() {
       this.authService.signInWithGithub()
          .then((res) => {
-            this.router.navigate(['dashboard'])
+            this.router.navigate([''])
          })
          .catch((err) => console.log(err));
     }
