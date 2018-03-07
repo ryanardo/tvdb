c4c4e4dbd27ea1fa6c7cb554987b7e09ad5ca171
Ryan G Ferris <ryanglenferris@gmail.com>
2018-03-06 16:21:06 -0800

    Delete api-keys.ts

--- api-keys.ts
+++
@@ -1,8 +0,0 @@
-export var masterFirebaseConfig = {
-  apiKey: "AIzaSyCR7Nv-cENXKWqJqoXaS6rEA-7WlpRx3j4",
-    authDomain: "tvdb-77970.firebaseapp.com",
-    databaseURL: "https://tvdb-77970.firebaseio.com",
-    projectId: "tvdb-77970",
-    storageBucket: "",
-    messagingSenderId: "157934628404"
-}
