795db4c61d1881447994a631049bfc5cd7792700
Guest User <Guest@Epicodus-4B.local>
2018-03-08 14:22:36 -0800

    finish favorites...for now

--- src/app/components/user-favorites/user-favorites.component.ts
+++ src/app/components/user-favorites/user-favorites.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
+import { TMDB_API } from './../../services/api/tmdb.service';
+
+
 
 @Component({
   selector: 'app-user-favorites',
   templateUrl: './user-favorites.component.html',
-  styleUrls: ['./user-favorites.component.css']
+  styleUrls: ['./user-favorites.component.css'],
+  providers: [TMDB_API]
 })
+
 export class UserFavoritesComponent implements OnInit {
 
-  constructor() { }
+  public href: string = "";
+  public id: string="";
+  public content;
+  public actor;
+  public similar;
+  public video;
+  public stringId;
+
+
+  moviesId: FirebaseListObservable<any[]>;
+  idArray: string[];
+  constructor(private database: AngularFireDatabase, private tmdb_api: TMDB_API) {
+    this.moviesId = this.database.list('userSave');
+
+
+  }
+
+
 
   ngOnInit() {
+    // for(let items in this.moviesId){
+    //   alert(items);
+    // }
+    // this.tmdb_api.getMovieById(this.moviesId[0]).subscribe(response => {
+    //   this.content=response.json()
+    //   console.log(this.moviesId);
+    // });
   }
 
 }
