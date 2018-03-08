c94237174e70cd90ba7f12fdd2a5adfb80c225d9
Ryan G Ferris <ryanglenferris@gmail.com>
2018-03-08 11:11:25 -0800

    Navbar Route
    Update the Navbarto not display when on the Login page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from './services/auth.service'
 import { routing } from './app.routing';
-import {AuthService} from './services/auth.service'
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css'] 
-
+	styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
-	constructor(public authService: AuthService){}
+	constructor(public authService: AuthService) { }
 	title = '';
 }
