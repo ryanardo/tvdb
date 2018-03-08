5cbf67380d99abef7b007446207608f47abf57ed
Ryan G Ferris <ryanglenferris@gmail.com>
2018-03-07 17:27:00 -0800

    Routing Homepage Navbar
    Update routing paths, methods, directives, etc. for the Homepage, and
    the Navbar, so the Homepage will display the API query results in
    search-results Component.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Router } from '@angular/router';
+import { TMDB_API } from './../../services/api/tmdb.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.css'],
+	providers: [TMDB_API]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+	movies: any[] = null;
 
-  ngOnInit() {
-  }
+	constructor(private tmdb_api: TMDB_API) { }
+
+	getSearchResults(query: string, adultContent: string) {
+		this.tmdb_api.getMultiSearch(query, adultContent).subscribe(response => {
+			this.movies = response.json();
+		});
+
+	}
+
+	ngOnInit() {
+	}
 
 }
